012e36b303277889dc8fd7965bcc17f54ab9fcf8
markan <markan@markanui-MacBook-Pro.local>
2019-04-19 11:40:41 +0900

    Updates

--- src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-import './Footer.css';
-
-const Footer = () => (
-    <div className="footer">Created by @asbear</div>
-  );
-  
-export default Footer;
--- src/pages/index.js
+++
@@ -1,4 +0,0 @@
-export { default as Insight } from './Insight';
-export { default as Home } from './Home';
-export { default as Post } from './Post';
-export { default as Witness } from './Witness';
