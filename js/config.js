21303f5ef86a1ec0f4edde5670d2d2a72d131d5d
anpigon <markan82@gmail.com>
2019-06-25 15:40:32 +0900

    Updates

--- js/config.js
+++ js/config.js
@@ -1,2 +1,2 @@
 const TISTORY_CLIENT_ID = "701428f0ab9255c05af72e994000c6e7";
-const TISTORY_REDIRECT_URI = "https://anpigon.github.io/steem/callback.html";
+const TISTORY_REDIRECT_URI = "https://anpigon.github.io/steeme/callback.html";
