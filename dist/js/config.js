51f2bb1acad4e9a062b478ccf50b685d8e769a9d
anpigon <markan82@gmail.com>
2019-06-28 11:13:52 +0900

    onCallback -> oncallback

--- dist/js/config.js
+++ dist/js/config.js
@@ -1,2 +1,2 @@
-const TISTORY_CLIENT_ID = "a3c1fbe6defdfa90877478c782ddf061";
-const TISTORY_REDIRECT_URI = "http://localhost:3000/callback.html";
+const TISTORY_CLIENT_ID = "701428f0ab9255c05af72e994000c6e7";
+const TISTORY_REDIRECT_URI = "https://anpigon.github.io/steeme/callback.html";
